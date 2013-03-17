68cc0b3e820647fa9f1c172d7082f54e1cf76aad
Alan Gutierrez <alan@prettyrobots.com>
2013-03-17 03:35:09 -0400

    Fix export of `say` and `die`.
    Closes #137.

---
+++ t/executable/say.t.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+var path = require('path'), fs = require('fs');
+
+require('./proof')(2, function (step, equal, execute, proof) {
+  step(function () {
+    execute('node', [ path.join(__dirname, 'say') ], '', step());
+  }, function (code, stdout, stderr) {
+    equal(code, 0, 'exit');
+    equal(stdout, '1..1\n# Hello\nok 1 1\n', 'said');
+  });
+});
