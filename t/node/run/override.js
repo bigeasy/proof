a1e3f2fbc47aef42c9a0df3e9e94548489dcd72f
Alan Gutierrez <alan@prettyrobots.com>
2014-09-21 17:28:15 -0400

    Remove pointless wrapper test.
    See #232.

--- t/node/run/override.js
+++
@@ -1,7 +0,0 @@
-module.exports = require("../../..")(function (equal) {
-    return {
-        equal: function (value, message) {
-            equal(value, "overridden", message)
-        }
-    }
-})
--- t/node/run/override.t.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-
-require("./override.js")(2, function (equal, ok) {
-    equal("overridden", "overridden")
-    ok(1, "ok")
-})
