6f3b2cc607c2d1321f96783fa5f584ef0c8b461a
Alan Gutierrez <alan@prettyrobots.com>
2014-09-22 00:30:29 -0400

    Test assertions.
    See #232.

---
+++ t/scaffold/assert.t.js
@@ -0,0 +1,27 @@
+var globals = Object.keys(global).concat([ 'errno' ])
+
+var expected = '\
+1..1\n\
+not ok 1 boolean\n\
+# { EXPECTED: true, GOT: false }\n\
+not ok 2 equal\n\
+# { EXPECTED: 2, GOT: 1 }\n\
+# expected 1\n\
+# passed   0\n\
+# failed   1\n\
+'
+
+require('../../redux')(1, function (assert) {
+    var stream = require('stream')
+    var scaffold = require('../../scaffold')
+    var stdout = new stream.PassThrough
+
+    scaffold(1, function (assert) {
+        assert(false, 'boolean')
+        assert(1, 2, 'equal')
+    }, globals, function () {}, {
+        stdout: stdout
+    })
+
+    assert(stdout.read().toString(), expected, 'assert')
+})
