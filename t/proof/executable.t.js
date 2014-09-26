4462a96a06a5643536592a5272a851444af78834
joshua pelletier <jgpelletier@gmail.com>
2014-09-26 15:49:13 -0700

    added assertion

--- t/proof/executable.t.js
+++ t/proof/executable.t.js
@@ -1,4 +1,10 @@
-require('../..')(1, function (assert) {
+require('../..')(2, function (assert) {
     var executable = require('../../executable')
+    var process = {
+        getuid: function () {
+                    return 701
+                }
+            }
     assert(executable(null, { mode: 0x1 }), 'other execute')
+    assert(executable(process, { mode: 0x40, uid: 701 }), 'other execute')
 })
