079039814104c31ec88ff79c1ec2b3930f4fa34b
joshua pelletier <jgpelletier@gmail.com>
2014-09-27 05:26:22 -0700

    working on 3rd assertion

--- t/proof/executable.t.js
+++ t/proof/executable.t.js
@@ -1,10 +1,16 @@
-require('../..')(2, function (assert) {
+require('../..')(3, function (assert) {
     var executable = require('../../executable')
     var process = {
+        //gid = 100,
         getuid: function () {
-                    return 701
-                }
-            }
+                return 701
+        },
+        getgid: function(gid) {
+                return gid
+        }
+    }
     assert(executable(null, { mode: 0x1 }), 'other execute')
     assert(executable(process, { mode: 0x40, uid: 701 }), 'other execute')
+    assert(executable(process, { mode: 0x1, gid: 100 }), 'other execute')
+    //assert(executable(process, { mode: 0x1 }), 'other execute')
 })
