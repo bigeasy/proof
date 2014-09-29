3666742d60c7cace490b1a1de308c4005bed4918
joshua pelletier <jgpelletier@gmail.com>
2014-09-29 09:32:26 -0700

    working on 4th assertion. changed getgid.

--- t/proof/executable.t.js
+++ t/proof/executable.t.js
@@ -1,13 +1,12 @@
-require('../..')(3, function (assert) {
+require('../..')(4, function (assert) {
     var executable = require('../../executable')
     var stats = { mode: 0x8, gid: 100 }
     var process = {
-        //gid = 100,
         getuid: function () {
                 return 701
         },
         getgid: function () {
-                return gid
+                return  stats  
         },
         getgroups: function () {
                  return [ 100, 33, 19 ]
@@ -17,7 +16,7 @@ require('../..')(3, function (assert) {
     assert(executable(null, { mode: 0x1 }), 'other execute')
     assert(executable(process, { mode: 0x40, uid: 701 }), 'other execute')
     assert(executable(process, stats), 'other execute')
-    //                  ^^^ DOES THIS NEED TO EXECPT THE 2ND ARG? 
-    console.log(process.getgroups && process.getgroups().some(function (gid) { return gid == stats.gid }))
-    //assert(executable(process, { mode: 0x1 }), 'other execute')
+    assert(executable(process, stats), 'other execute') // does not pass
+    console.log(process.getgid() == stats.gid && stats.mode & 0x8) 
+    // ^^^ returns false 
 })
