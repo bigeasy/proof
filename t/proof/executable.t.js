c01c73a3d94c9c7845f654566df69ceaa43b564a
joshua pelletier <jgpelletier@gmail.com>
2014-09-29 15:02:28 -0700

    testing variables with console.log

--- t/proof/executable.t.js
+++ t/proof/executable.t.js
@@ -1,14 +1,13 @@
 require('../..')(4, function (assert) {
     var executable = require('../../executable')
-    var stats = { mode: 0x8, gid: 100 }
-     // ^^^ named `stat` not `stats`.
+    var stat = { mode: 0x8, gid: 100 }
     var groups = [ 100, 33, 19 ]
     var process = {
         getuid: function () {
                 return 701
         },
         getgid: function () {
-                return stats.gid 
+                return stat.gid 
         },
         getgroups: function () {
                 return groups
@@ -17,9 +16,15 @@ require('../..')(4, function (assert) {
     }
     assert(executable(null, { mode: 0x1 }), 'other execute')
     assert(executable(process, { mode: 0x40, uid: 701 }), 'other execute')
-    assert(executable(process, stats), 'other execute')
-    assert(executable(process, stats), 'other execute') // does not pass
-    // how do I return both of the object elements?
-    console.log(process.getgid() == stats.gid && stats.mode & 0x8) // returns 8 
-    //order of operation         ^^ order 9   ^^ order 11   ^^order 10
+    assert(executable(process, stat), 'other execute')
+    assert(executable(process, stat), 'other execute') // does not pass
+
+    // vvv Testing variables:
+    console.log((stat.mode & 0x8) && (process.getgid() == stat.gid)) // returns 2nd test: true
+    //order of operation  ^10   ^^ order 11         ^^order 9
+    console.log((process.getgid() == stat.gid) && (stat.mode & 0x8)) // returns 2nd test: 8 
+    console.log("stat.git",stat.gid)
+    console.log("process.getgid()", process.getgid())
+    console.log("stat.mode", stat.mode)
+    console.log(stat.mode == 0x8)
 })
