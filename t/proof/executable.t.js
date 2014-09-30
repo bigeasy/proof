554c91ad9758cdc926c9115fbe60a134738600d2
joshua pelletier <jgpelletier@gmail.com>
2014-09-30 09:24:24 -0700

    1 test fails but the report shows most coverage

--- t/proof/executable.t.js
+++ t/proof/executable.t.js
@@ -1,17 +1,18 @@
 require('../..')(4, function (assert) {
     var executable = require('../../executable')
-    var stat = { mode: 0x8, gid: 100 }
-    var groups = [ 100, 33, 19 ]
+    //var stat = { mode: 0x8, gid: 100 }
+    //var groups = [ 100, 33, 19 ]
+    // var process = {}
+    //process.setuid(701)
+    console.log(process.getuid())
 
+    // passsing this set of tests results in the most coverage. The stat.mode branches
+    // in the 2nd and 4th assertion, however, are not covered. The 2nd assertion
+    // does not pass, while the rest do.
     assert(executable(null, { mode: 0x1 }), 'other execute')
     assert(executable(process, { mode: 0x40, uid: 701 }), 'other execute')
-    assert(executable(process, stat), 'other execute')
-    assert(executable(process, { mode: 0x8, gid: 100 }), 'other execute') // does not pass
-    // There is more covarage if the 2nd and 4th conditional are changed in `executable.js`
-    // but that is not allowed. It does mean, however, that consideration must be paid to 
-    // the order of the test. 
-    // QUESTION: how come order of the conditionals in `executable.js` 
-    // matters? 
+    assert(executable(process, { mode: 0x8, gid: 100 }), 'other execute')
+    assert(executable(process, { mode: 0x8, gid: 100 }), 'other execute')
 })
 
 // may need to reuse this vvv.
