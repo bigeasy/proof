3cc1d1f43224da9ed58ea36de59aa8e5875b28b1
Joshua Pelletier <jgpelletier@gmail.com>
2014-10-02 15:17:15 +0000

    removed and added comments

--- t/proof/executable.t.js
+++ t/proof/executable.t.js
@@ -1,34 +1,28 @@
 require('../..')(4, function (assert) {
     var executable = require('../../executable')
-    //var stat = { mode: 0x8, gid: 100 }
-    var groups = [ 100, 33, 19 ]
-    // var process = {}
-    //process.setuid(701)
-    //console.log(process.getuid())
+    var stat = { mode: 0x8, gid: 100 }
+    var groups = [ 100, 33, 19, 10]
     var process = {
         getuid: function () {
-            return 701
+            return 700
         },
         getgid: function () {
-            return 100
+            return 10
         },
         getgroups: function () {
             return groups
         }
     }
 
-    // passsing this set of tests results in the most coverage. The stat.mode branches
-    // in the 2nd and 4th assertion, however, are not covered. The 2nd assertion
-    // does not pass, while the rest do.
     assert(executable(null, { mode: 0x1 }), 'other execute')
         // what is a bit mask?
-        // you should translate 0x40 etc. to a bit mask.
-    assert(executable(process, { mode: 0x40, uid: 701 }), '*who* executes?')
-                            // ^^^ what does this test?
-    assert(executable(process, { mode: 0x8, gid: 100 }), '*who* executes?')
-                            // ^^^ what does this test?
-    assert(executable(process, { mode: 0x8, gid: 100 }), 'other execute')
-        // ^^^ trying to get the fourth branch.
+        // bit mask  0x1: 000000001
+        // bit mask 0x40: 001000000
+        // bit mask  0x8: 000001000
+    assert(executable(process, { mode: 0x40, uid: 700 }), 'user execute')
+    assert(executable(process, { mode: 0x8, gid: 10 }), 'group executes')
+    assert(executable(process, { mode: 0x8, gid: 10 }), '`who` executes?')
+
         // '10111101001' & '1000'
         //
         // value & mask == mask <- is set?
@@ -46,5 +40,3 @@ require('../..')(4, function (assert) {
         //~'11111110111' == ~'1000'
         // '10111100001' <-
 })
-
-// may need to reuse this vvv.
