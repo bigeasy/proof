967db3da7e3bd95182a886b26af8138eb75075d4
joshua pelletier <jgpelletier@gmail.com>
2014-10-02 10:39:59 -0700

    fixed code

--- t/proof/executable.t.js
+++ t/proof/executable.t.js
@@ -1,4 +1,4 @@
-require('../..')(5, function (assert) {
+require('../..')(4, function (assert) {
     var executable = require('../../executable')
     var stat = { mode: 0x8, gid: 100 }
     var groups = [ 100, 33, 19, 10]
@@ -24,16 +24,18 @@ require('../..')(5, function (assert) {
     }
 
     assert(executable(null, { mode: 0x1 }), 'other execute')
-        // what is a bit mask?
-        // bit mask  0x1: 000000001
-        // bit mask 0x40: 001000000
-        // bit mask  0x8: 000001000
     assert(executable(process, { mode: 0x40, uid: 700 }), 'user execute')
     assert(executable(process, { mode: 0x8, gid: 10 }), 'group executes')
     assert(executable(process2, { mode: 0x8, gid: 10 }), 'group executes')
 
     // vvv is covered, but returns false so the test fails.
-    //assert(executable(process2, { mode: 0x180, gid: 19}), 'returns false')
+    // assert(executable(process2, { mode: 0x180, gid: 19}), 'returns false')
+
+// LESSON:
+    // what is a bit mask?
+    // bit mask  0x1: 000000001
+    // bit mask 0x40: 001000000
+    // bit mask  0x8: 000001000
 
         // '10111101001' & '1000'
         //
