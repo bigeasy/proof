a56c96c6743e9bfec098763af9f0ba82f63c2927
joshua pelletier <jgpelletier@gmail.com>
2014-10-02 17:48:20 -0700

    Removed comments.

--- executable.js
+++ executable.js
@@ -1,9 +1,9 @@
 module.exports = function (process, stat) {
-    if (stat.mode & 001) return true // was 0x1
-    if (stat.uid == process.getuid() && stat.mode & 010) return true // was 0x40
+    if (stat.mode & 001) return true
+    if (stat.uid == process.getuid() && stat.mode & 010) return true
     if (process.getgroups && process.getgroups().some(function (gid) {
         return gid == stat.gid
-    }) && stat.mode & 100) return true // was 0x8
-    if (process.getgid() == stat.gid && stat.mode & 100) return true // was 0x8
+    }) && stat.mode & 100) return true
+    if (process.getgid() == stat.gid && stat.mode & 100) return true
     return false
 }
