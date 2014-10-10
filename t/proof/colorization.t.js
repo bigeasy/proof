0b4cdfa1e334980ec64acb9232d98445ab9baed4
joshua pelletier <jgpelletier@gmail.com>
2014-10-10 14:47:16 -0700

    Asserted colorization options are true

--- t/proof/colorization.t.js
+++ t/proof/colorization.t.js
@@ -4,7 +4,9 @@ require('../..')(1, function (assert) {
     var colorization = require('../../colorization')
 
     var options = { params: { monochrome: true } }
+    var colors = colorization(options)
 
-    assert(options.params.monochrome == true, 'options.params.monochrome')
+    assert(colorization(options), 'options true')
+
 
 })
