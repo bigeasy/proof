d1a30b36994f7991d49bce4eb6be3534a7a77b22
joshua pelletier <jgpelletier@gmail.com>
2014-10-10 14:52:21 -0700

    Asserted colors red object has monochrome red.

--- t/proof/colorization.t.js
+++ t/proof/colorization.t.js
@@ -6,7 +6,9 @@ require('../..')(1, function (assert) {
     var options = { params: { monochrome: true } }
     var colors = colorization(options)
 
+    //assert(options.params.monochrome == true, 'options.params.monochrome')
     assert(colorization(options), 'options true')
+    assert(colors.red, 'has monochrome red')
 
 
 })
