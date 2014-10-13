60cec8e9a260e6ca442d53ff974e6a93ef840d33
joshua pelletier <jgpelletier@gmail.com>
2014-10-13 08:19:04 -0700

    Assert monochrome.

--- t/proof/colorization.t.js
+++ t/proof/colorization.t.js
@@ -1,20 +1,15 @@
 #!/usr/bin/env node
 
-require('../..')(2, function (assert) {
+require('../..')(5, function (assert) {
     var colorization = require('../../colorization')
 
     var options = { params: { monochrome: true } }
     var colors = colorization(options)
 
-    //assert(options.params.monochrome == true, 'options.params.monochrome')
     assert(colorization(options), 'options true')
-    assert(colors.red, 'has monochrome red')
-    //assert(colorization(colors), 'has red')
-    /*
-    options = { params: { monochrome: false } }
-    colors = colorization(options)
-    assert(colorization(options), 'no options')
-    assert(colors.red, 'has red')
-    */
+    assert(colors.red('test'), 'has monochrome red')
+    assert(colors.green('test'), 'has monochrome green')
+    assert(colors.blue('test'), 'has monochrome blue')
+    assert(colors.grey'test'), 'has monochrome grey')
 
 })
