f563deb57c4c8826a1e80826daa2335c448c4a6f
Alan Gutierrez <alan@prettyrobots.com>
2014-10-13 15:35:41 -0400

    Instruction toward a better colorization test.

--- t/proof/colorization.t.js
+++ t/proof/colorization.t.js
@@ -6,8 +6,21 @@ require('../..')(10, function (assert) {
     var options = { params: { monochrome: true } }
     var colors = colorization(options)
 
+/*
+    Uncomment and learn the different ways to invoke `assert`.
+
+    assert(true, 'ok')              // boolean
+    assert(false, 'not okay')       // failed boolean
+    assert(1, 1, 'equal')           // equality
+    assert(1, 2, 'equal')           // failed equality
+    assert({ a: 1 }, { a: 2 }, 'deep equal') // failed deep equal, (equal is a deep equal).
+*/
+
     assert(colorization(options), 'options true')
     assert(colors.red('test'), 'has monochrome red')
+                           //  ^^^ what should `colors.red` return?
+//    assert(colors.red('test'), 'argle-bargle', 'has monochrome red')
+                             //  ^^^ does it return "argle-bargle"?
     assert(colors.green('test'), 'has monochrome green')
     assert(colors.blue('test'), 'has monochrome blue')
     assert(colors.gray('test'), 'has monochrome gray')
