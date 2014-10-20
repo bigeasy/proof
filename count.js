18ceb419a36834f424c199bee3f81a35f98051ad
joshua pelletier <jgpelletier@gmail.com>
2014-10-20 14:24:37 -0700

    Working on catching an error.

--- count.js
+++ count.js
@@ -1,12 +1,23 @@
 module.exports = function (arr) {
-    var i = 0 
-    var a = []
-    for (i; i < arr.length; i++) {
-        if (typeof(arr[i]) != 'string') {
-            return console.log( arr[i] + " element not a string.")
-        } else {
-            a.push(arr[i].length, arr[i])
-        }   
+    var e = new Error('Array element not a string')
+
+    function addLength(arr, e) {
+        var e = new Error('Array element not a string')
+        var a = []
+        var i = 0 
+        for (i; i < arr.length; i++) {
+            if (typeof(arr[i]) != 'string') {
+                throw e
+            } else {
+                a.push(arr[i].length, arr[i])
+            }   
+        }
+        return a
     }
-    return a
+
+    try {
+        var a = addLength(arr)
+        return a
+    } catch (e) {throw e}
+
 }
--- t/proof/count.t.js
+++ t/proof/count.t.js
@@ -4,6 +4,6 @@ require('../..')(2, function (assert) {
     var count = require('../../count')
     var arr = [ "one", "two", "three" ]
     var arr2 = [ "one", "two", 3 ]
-    assert(count(arr), [ 3, 'one', 3, 'two', 5, 'three' ], 'ok')
-    assert(!count(arr2), true, 'ok')
+    assert(count(arr), [ 3, 'one', 3, 'two', 5, 'three' ], 'Add lengths')
+    assert(count(arr2), e, 'tests a failure')
 })
