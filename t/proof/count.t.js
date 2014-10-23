91cac82e574fbc671c3da310a13de2296cabf879
joshua pelletier <jgpelletier@gmail.com>
2014-10-23 07:16:09 -0700

    Added comments for clarification.

--- t/proof/count.t.js
+++ t/proof/count.t.js
@@ -5,10 +5,25 @@ require('../..')(1, function (assert) {
     var arr = [ "one", "two", "three" ]
     assert(count(arr), [ 3, 'one', 3, 'two', 5, 'three' ], 'Add lengths')
    
-    /*
     var arr2 = [ "one", "two", 3 ]
+
+    // When working with assert, how do I position try/catch so it both catches the error
+    // and allows assert to eveluate? 
     try { 
-    assert.throws(!count(arr2) , e , 'throws an exception')
-    } catch (e) { console.log(e) }
-    */
+    // How does the throws method work? It isn't catching exceptions.   
+        assert/*.throws*/(count(arr2), e, 'throws an exception')
+    } catch (e) {
+        console.log(e)
+    }
+    // MY LOGIC: 
+    // With the exception being thrown from count, and the catch being placed after the test,
+    // the stack starts to unwind before the evaluation of assert. This error must be caught
+    // before the the assertion test evaluates, otherwise the test will continue to fail.
+    //
+    // FAILED ATTEMPTS: 
+    //  - Place try/catch around assert. 
+    //  - Place try/catch within assert so it tries count and catches before the evaluation.
+    //  - Assign the try/ catch block to a variable. 
+    //  - Use the the throws method.
+    //  - Try multiple expected values like: e / Error / [Error: Array element not a string.]
 })
