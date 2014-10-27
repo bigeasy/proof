4086c97c583732450c0878de97eea7a1f29274e8
joshua pelletier <jgpelletier@gmail.com>
2014-10-27 11:55:55 -0700

    Fixed formatting.

--- t/proof/count.t.js
+++ t/proof/count.t.js
@@ -3,13 +3,13 @@
 require('../..')(2, function (assert) {
     var count = require('../../count')
     var arr = [ "one", "two", "three" ]
-    assert(count(arr), [ 3, 'one', 3, 'two', 5, 'three' ], 'Add lengths')
+    assert(count(arr), [ 3, 'one', 3, 'two', 5, 'three' ], 'add lengths')
    
     var arr2 = [ "one", "two", 3 ]
 
     try { 
         count(arr2)
     } catch (e) {
-        assert(e.message, 'Array element not a string.', 'Throws expected exception')
+        assert(e.message, 'Array element not a string.', 'throws expected exception')
     }
 })
