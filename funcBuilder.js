a188a3ffb9526330c6d0446a61fd7063f8bafa27
joshua pelletier <jgpelletier@gmail.com>
2014-10-28 14:31:36 -0700

    Used array join method.

--- funcBuilder.js
+++ funcBuilder.js
@@ -1,10 +1,6 @@
 module.exports = function (func) {
     return function (arr) {
-    // Use the array join method.
-        var a = func(arr)
-        var lastElement = a.pop()
-        var push = lastElement.concat('\n') 
-        a.push(push)
-        return a.toString().replace(/,/g, '\n')
+        var a = func(arr).join() + ('\n')
+        return a.replace(/,/g, '\n')
     } 
 }
