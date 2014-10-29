f0bed82795ce74df42d443b6ef6598703c94231f
joshua pelletier <jgpelletier@gmail.com>
2014-10-29 10:23:02 -0700

    Close #259.

--- adaptor.js
+++ adaptor.js
@@ -1,5 +1,7 @@
+// This generates a function, which accepts a single argument (what do `you` mean as a scalar argument?)
 module.exports = function (func) {
-    return function (arr) {
+    return function (arg) { // <- wrapper
+        var arr = [ arg ]
         return func(arr).join('\n') + ('\n')
     } 
 }
--- t/proof/adaptor.t.js
+++ t/proof/adaptor.t.js
@@ -3,8 +3,8 @@
 require('../..')(1, function (assert) {
     var count = require('../../count')
     var adaptor = require('../../adaptor')
-    var arr = ["string"]
+    var arg = "string"
     var string = '6\nstring\n'
     var test = adaptor(count)
-    assert(test(arr), string, 'equal')
+    assert(test(arg), string, 'equal')
 })
