84ff6568c670523e6c26f9bf309a221d83df1cdb
joshua pelletier <jgpelletier@gmail.com>
2014-11-03 15:48:51 -0800

    Added a stream.

--- t/proof/printer.t.js
+++ t/proof/printer.t.js
@@ -2,5 +2,9 @@
 
 require('../..')(1, function (assert) {
     var printer = require('../../printer')
+    var stream = require('stream')
+    var test = new stream.PassThrough
+    test.write('foo')
     assert(typeof(printer), 'function', 'ok') 
+    assert(test.read().toString(), 'foo', 'equal')
 })
