4cb17261b34ad316cc3c18e73865941a3f338ed3
joshua pelletier <jgpelletier@gmail.com>
2014-11-03 07:13:11 -0800

    Changed control flow to ternary operator.

--- count.js
+++ count.js
@@ -1,8 +1,3 @@
 module.exports = function (arg) {
-        if (arg == null) {
-            return [] // this is returned when no arguments. see adaptor test.
-        } else {
-            arg = String(arg)
-            return [arg.length, arg]
-        }
+    return (arg == null) ? [] : [String(arg).length, String(arg)]
 }
