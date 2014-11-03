dc4de8cdcd5e0864771c0b923c2e519540b3b27d
joshua pelletier <jgpelletier@gmail.com>
2014-11-03 14:07:30 -0800

    Removed branch that returns a newline for an array with an empty string.

--- adaptor.js
+++ adaptor.js
@@ -1,16 +1,10 @@
-module.exports = function (func) { // adaptor cannot be wrapper specific.
+module.exports = function (func) {
     return function (arg) {
         var arr = func(arg)
 
         if (arr.length == 0) {
             return ('')
         } 
-        
-        for (var i = 0; i < arr.length; i++) {
-            if (String(arr[i]).length == 0) {
-                return ('\n')
-            }
-        }
         
         return arr.join('\n') + ('\n')
 
--- t/proof/adaptor.t.js
+++ t/proof/adaptor.t.js
@@ -1,13 +1,11 @@
 #!/usr/bin/env node
 
-require('../..')(3, function (assert) {
+require('../..')(2, function (assert) {
     var count = require('../../count')
     var adaptor = require('../../adaptor')
     var arg = "string"
     var string = '6\nstring\n'
-    var empty = ''
     var test = adaptor(count)
     assert(test(arg), string, 'equal')
     assert(test(), '', 'equal')
-    assert(test(empty), '\n', 'equal')
 })
