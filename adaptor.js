245cfb16c43e5521165b64ddf6a6e89dfa7fdfac
joshua pelletier <jgpelletier@gmail.com>
2014-11-03 10:50:15 -0800

    Working on 3rd assertion.

--- adaptor.js
+++ adaptor.js
@@ -1,5 +1,7 @@
-module.exports = function (func) {
+module.exports = function (func) { // adaptor cannot be wrapper specific.
     return function (arg) {
         return ((func(arg).join('\n') + ('\n')) == '\n' ) ? ('') : func(arg).join('\n') + ('\n') 
+        //if (func(arg).join('\n') + ('\n')) == '\n' ) return ('')
+        //else if (
     } 
 }
--- t/proof/adaptor.t.js
+++ t/proof/adaptor.t.js
@@ -1,11 +1,17 @@
 #!/usr/bin/env node
 
-require('../..')(2, function (assert) {
+require('../..')(3, function (assert) {
     var count = require('../../count')
     var adaptor = require('../../adaptor')
     var arg = "string"
     var string = '6\nstring\n'
+    var empty = ''
     var test = adaptor(count)
+    //console.log(count(empty)) // [ 0, '' ]
+    //console.log(count(newline)) // [ 1, '\n' ]
+    //console.log(test(empty)) // '0\n\n'
     assert(test(arg), string, 'equal')
     assert(test(), '', 'equal')
+    assert(test(empty), '0\n\n', 'equal') // <- an empty string needs to return a newline
+                                          //    No 0 and no additoinal newline
 })
