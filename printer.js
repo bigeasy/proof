59be3316a416fe289cef9930e03062fe4546337a
joshua pelletier <jgpelletier@gmail.com>
2014-11-03 17:40:19 -0800

    Added echo function and assertion test.

--- printer.js
+++ printer.js
@@ -1,4 +1,5 @@
 module.exports = function (func) {
     return function(arg) {
+        return func(arg)
     }
 }
--- t/proof/printer.t.js
+++ t/proof/printer.t.js
@@ -1,10 +1,14 @@
 #!/usr/bin/env node
 
-require('../..')(1, function (assert) {
+require('../..')(2, function (assert) {
     var printer = require('../../printer')
     var stream = require('stream')
     var test = new stream.PassThrough
     test.write('foo')
+    function echo (string) { return string }
+    var print = printer(echo)
+    console.log(print(test.read().toString()))
     assert(typeof(printer), 'function', 'ok') 
-    assert(test.read().toString(), 'foo', 'equal')
+    assert(print(test.read().toString()), 'foo', 'equal')
+
 })
