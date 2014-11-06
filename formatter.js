2609b2acebdb1bb86e9c810755538be0b2e6a6ab
joshua pelletier <jgpelletier@gmail.com>
2014-11-06 10:03:02 -0800

    Joins with an empty string instead of newline.

--- formatter.js
+++ formatter.js
@@ -1,12 +1,6 @@
 module.exports = function (func) {
     return function (arg) {
         var arr = func(arg)
-
-        if (arr.length == 0) {
-            return ('')
-        } 
-        
-        return arr.join('\n') + ('\n')
-
+        return arr.join('')
     }
 }
