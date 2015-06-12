af2a0f71d40751b6f3ae7812d5971c4a41a9d4b7
Alan Gutierrez <alan@prettyrobots.com>
2015-06-12 15:26:34 -0500

    Convert `shebang.js` to Cadence Redux.

--- shebang.js
+++ shebang.js
@@ -1,5 +1,5 @@
 var fs = require('fs'), path = require('path'), spawn = require('child_process').spawn
-var cadence = require('cadence')
+var cadence = require('cadence/redux')
 var candidate = require('./candidate')
 var executable = require('./executable')
 
@@ -7,7 +7,7 @@ var executable = require('./executable')
 // don't want to go digging into it a this point. Not that difficult, since
 // there are command line utilities we can invoke to get a list of file
 // associations.
-module.exports = cadence(function (step, platform, program, parameters) {
+module.exports = cadence(function (async, platform, program, parameters) {
     var isWindows = ! platform.indexOf('win')
     if (isWindows) {
         switch (path.extname(program)) {
@@ -16,8 +16,8 @@ module.exports = cadence(function (step, platform, program, parameters) {
         case '.cmd':
             return [ program, parameters ]
         default:
-            step(function () {
-                fs.readFile(program, step())
+            async(function () {
+                fs.readFile(program, async())
             }, function (buffer, stat) {
                 var first, $
                 if (buffer[0] == 0x23 && buffer[1] == 0x21
@@ -29,7 +29,7 @@ module.exports = cadence(function (step, platform, program, parameters) {
                         parameters.unshift(program)
                         program = $[1]
                     }
-                    candidate(program, step())
+                    candidate(program, async())
                 } else {
                     return [ 'node' ]
                 }
@@ -38,18 +38,22 @@ module.exports = cadence(function (step, platform, program, parameters) {
             })
         }
     } else {
-        step(function () {
-            fs.stat(program, step())
+        async(function () {
+            fs.stat(program, async())
         }, function (stat) {
-            if (executable(process, stat)) return [ step, program, parameters ]
-            else fs.readFile(program, step())
-        }, function (buffer) {
-            // no exec bit, but there's a shebang line? I call shenanigans.
-            if (buffer[0] == 0x23 && buffer[1] == 0x21) {
-                throw new Error('set execute bit to use shebang line')
+            if (executable(process, stat)) {
+                return [ program, parameters ]
             }
-            parameters.unshift(program)
-            return [ 'node', parameters ]
+            async(function () {
+                fs.readFile(program, async())
+            }, function (buffer) {
+                // no exec bit, but there's a shebang line? I call shenanigans.
+                if (buffer[0] == 0x23 && buffer[1] == 0x21) {
+                    throw new Error('set execute bit to use shebang line')
+                }
+                parameters.unshift(program)
+                return [ 'node', parameters ]
+            })
         })
     }
 })
