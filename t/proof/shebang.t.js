5b168fe88ea1d22da460f82f7892d09fd93990b5
Alan Gutierrez <alan@prettyrobots.com>
2015-06-15 00:32:26 -0500

    Test shebang line without execute bit.

--- t/proof/shebang.t.js
+++ t/proof/shebang.t.js
@@ -1,10 +1,16 @@
-require('../..')(2, require('cadence')(function (step, assert) {
+require('../..')(3, require('cadence/redux')(prove))
+
+function prove (async, assert) {
     var path = require('path')
     var shebang = require('../../shebang'), path = require('path')
-    step(function () {
-        shebang('unix', path.join(__dirname, 'fixtures/shebang/first/program'), [], step())
+    async(function () {
+        shebang('unix', path.join(__dirname, 'fixtures/shebang/first/program'), [], async())
     }, function (program, parameters) {
         assert(path.basename(program), 'program', 'shebang program')
         assert(parameters, [], 'shebang parameters')
-    })
-}))
+    }, [function () {
+        shebang('unix', path.join(__dirname, 'fixtures/shebang/unset'), [], async())
+    }, function (error) {
+        assert(error.message, 'set execute bit to use shebang line', 'set execute bit')
+    }])
+}
