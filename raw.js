4c55f15850a32f89295637489e317d878f83e997
Alan Gutierrez <alan@prettyrobots.com>
2015-07-30 11:37:19 -0500

    Remove stray letter "t".

--- raw.js
+++ raw.js
@@ -2,7 +2,7 @@ module.exports = function () {
     var events = []
     return function (event) {
         events.push(event)
-        if (event.type === 'eof') t{
+        if (event.type === 'eof') {
             return [ JSON.stringify(events, null, 2), '\n' ]
         }
         return []
