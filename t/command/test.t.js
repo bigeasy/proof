7928c0c17de8fc37c7a140f56fdab0f10e87d850
Alan Gutierrez <alan@prettyrobots.com>
2015-09-09 09:35:31 -0500

    100% test coverage of test.
    See #224.

---
+++ t/command/test.t.js
@@ -0,0 +1,29 @@
+var output = '\
+ ✓ t/command/fixtures/ok ............................... (1/1) 0.XXX Success\n\
+                                  tests (1/1) assertions (1/1) 0.XXX Success\n'
+var cadence = require('cadence')
+
+var prove = cadence(function (async, assert) {
+    var stream = require('stream')
+    var proof = require('../../proof.bin.js')
+    var stdout = new stream.PassThrough
+    var stdin = new stream.PassThrough
+    var stderr = new stream.PassThrough
+    async(function () {
+        proof({}, [ 'test', '-p', '1', '-M', 't/command/fixtures/ok' ], {
+            stdin: stdin,
+            stderr: stderr,
+            stdout: stdout
+        }, async())
+    }, function (code) {
+        assert(stderr.read(), null, 'stderr')
+        assert(stdout.read().toString().replace(/\d{3}/g, 'XXX'), output, 'stdout')
+        assert(code, 0, 'ran')
+    }, [function () {
+        proof({}, [ 'test', '-h' ], {}, async())
+    }, function (error) {
+        assert(error.message, 'help', 'help')
+    }])
+})
+
+require('../..')(4, prove)
--- test.js
+++ test.js
@@ -3,36 +3,30 @@ var spawn = require('child_process').spawn
 var path = require('path')
 var Delta = require('delta')
 
-function test (options) {
-    _test(options, function (error) { if (error) throw error })
-}
-
-var _test = cadence(function (async, options) {
+var test = cadence(function (async, options) {
+    if (options.param.help) options.help()
     var executable = path.join(__dirname, 'proof.bin.js')
     var progress = [ executable, 'progress' ], run = [ executable, 'run' ]
     options.given.forEach(function (name) {
-        if ('help' == name) {
-            options.usage()
-        } else if (/^(monochrome|width|digits)$/.test(name)) {
+        if (/^(monochrome|width|digits)$/.test(name)) {
             progress.push('--' + name, options.params[name])
         } else {
             run.push('--' + name, options.params[name])
         }
     })
     run.push.apply(run, options.argv)
-    progress = spawn('node',  progress, { stdio: [ 'pipe', process.stdout, process.stderr ] })
-    run = spawn('node', run, { stdio: [ 'pipe', 'pipe', process.stderr ] })
+    progress = spawn('node',  progress, { stdio: [ 'pipe', 'pipe', 'pipe' ] })
+    run = spawn('node', run, { stdio: [ 'pipe', 'pipe', 'pipe' ] })
     run.stdout.pipe(progress.stdin)
+    run.stderr.pipe(options.stderr)
+    progress.stdout.pipe(options.stdout)
+    progress.stderr.pipe(options.stderr)
     async(function () {
         var delta = new Delta(async())
         delta.ee(run).on('close')
         delta.ee(progress).on('close')
     }, function (runCode, runSignal, progressCode, progressSignal) {
-        async(function () {
-            new Delta(async()).ee(process).on('exit')
-        }, function () {
-            process.exit(runCode || progressCode)
-        })
+        return runCode || progressCode
     })
 })
 
