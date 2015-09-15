0479adff4bfa4e100048b54e86d4a51faa2c398a
Alan Gutierrez <alan@prettyrobots.com>
2015-09-15 01:18:01 -0500

    Coverage of width of formatting.
    See #224.

---
+++ convert.js
@@ -0,0 +1,2 @@
+process.stdout.write(JSON.stringify(process.argv[2] + '\n'))
+process.stdout.write('\n')
--- t/command/progress.t.js
+++ t/command/progress.t.js
@@ -5,25 +5,29 @@ var prove = cadence(function (async, assert) {
     var fs = require('fs')
     var path = require('path')
     var stream = require('stream')
-    var test = cadence(function (async, name, exit, stdout) {
+    var test = cadence(function (async, name, exit, argv, stdout) {
         stdout || (stdout = new stream.PassThrough)
         var stdin = new stream.PassThrough
         var input = fs.readFileSync(path.join(__dirname, 'fixtures', name + '.in.txt'), 'utf8')
         var output = fs.readFileSync(path.join(__dirname, 'fixtures', name + '.progress.out.txt'), 'utf8')
         async(function () {
-            proof({ env: {} }, [ 'progress', '-t', '-M' ], { stdin: stdin, stdout: stdout }, async())
+            proof({ env: {} }, [ 'progress', '-t', '-M' ].concat(argv || []), { stdin: stdin, stdout: stdout }, async())
             stdin.write(input)
             stdin.end()
         }, function (code) {
-            assert(stdout.read().toString().replace(/\d{3}/g, 'XXX'), JSON.parse(output), name)
+            assert(stdout.read().toString(), JSON.parse(output), name)
             assert(code, exit, name + ' exit')
         })
     })
     async(function () {
         test('success', 0, async())
     }, function () {
+        test('narrow', 0, [ '--digits', '1' ], async())
+    }, function () {
+        test('wide', 0, [ '--digits', '11' ], async())
+    }, function () {
         test('bailout', 1, async())
     })
 })
 
-require('../..')(4, prove)
+require('../..')(8, prove)
