e217e98d4daafd305b3817194c860ce7d1f25c35
Alan Gutierrez <alan@prettyrobots.com>
2015-09-21 10:09:28 -0500

    100% test coverage.
    Closes #224.

--- t/proof/parse.t.js
+++ t/proof/parse.t.js
@@ -1,5 +1,32 @@
-require('../..')(1, prove)
+var cadence = require('cadence')
 
-function prove (assert) {
-    assert(require('../../parse'), 'require')
-}
+var prove = cadence(function (async, assert) {
+    var parse = require('../../parse')
+    var fs = require('fs')
+    var path = require('path')
+    var stream = require('stream')
+    var stderr
+
+    async(function () {
+        var input = fs.readFileSync(path.join(__dirname, 'fixtures', 'type.in.txt'), 'utf8')
+        var stdin = new stream.PassThrough
+        stderr = new stream.PassThrough
+        parse({ stdin: stdin, stderr: stderr }, function () { }, async())
+        stdin.write(input)
+        stdin.end()
+    }, function () {
+        assert(stderr.read().toString(),
+            'error: cannot parse runner output at line 3: unknown line type oops\n', 'unknown type')
+        var input = fs.readFileSync(path.join(__dirname, 'fixtures', 'exit.in.txt'), 'utf8')
+        var stdin = new stream.PassThrough
+        stderr = new stream.PassThrough
+        parse({ stdin: stdin, stderr: stderr }, function () { }, async())
+        stdin.write(input)
+        stdin.end()
+    }, function () {
+        assert(stderr.read().toString(),
+            'error: cannot parse runner test exit code at line 4: exit code -\n', 'bad exit code')
+    })
+})
+
+require('../..')(2, prove)
