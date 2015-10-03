9ec01ad50a63c61211e2ab6e7ff36c73eceae114
Alan Gutierrez <alan@prettyrobots.com>
2015-10-02 21:09:30 -0500

    Remove `t/executable` tests.
    The are now duplicated by the test of Arguable based command line
    parser.

--- t/executable/leak.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-var test = require('../..')
-
-a = 1
-
-test(1, function (ok) { ok(true, 'test truth') })
--- t/executable/timeout.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-
-require('../..')(1, function (callback) {
-    setTimeout(function () { callback() }, 6e4)
-})
