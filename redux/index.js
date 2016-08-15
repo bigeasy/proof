0c95099cf36af9dfe5eac675d613755d34b60664
Alan Gutierrez <alan@prettyrobots.com>
2016-08-15 12:53:36 -0500

    Reintroduce half-baked module export.
    See #300.
    See #293.

--- redux/index.js
+++ redux/index.js
@@ -1,6 +1,22 @@
 var scaffold = require('./scaffold')
 var globals = Object.keys(global).concat([ 'errno' ])
 
-module.exports = function (count, test) {
-    scaffold(count, require('../exit'), test)(globals, process.stdout)
+module.exports = function (module, count, test) {
+    var isMainModule
+    if (typeof module == 'object') {
+        isMainModule = require.main === module
+    } else {
+        isMainModule = true
+        test = count
+        count = module
+        module = {}
+    }
+    if (typeof count != 'number') {
+        test = count
+        count = 0
+    }
+    var prove = module.exports = scaffold(count, require('../exit'), test)
+    if (isMainModule) {
+         prove(globals, process.stdout)
+    }
 }
