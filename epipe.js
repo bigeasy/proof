50b26f0de3b4ce6515fd5d997ec47f771c9ababc
Alan Gutierrez <alan@prettyrobots.com>
2017-03-31 01:10:23 -0500

    Remove `epipe.js`.
    See #317.

--- epipe.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (exitable) {
-    return function (error) {
-        if (error.code == 'EPIPE') {
-            exitable.exit(1)
-        } else {
-            throw error
-        }
-    }
-}
