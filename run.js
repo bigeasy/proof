812186ecbf22629b1b6b79e60fa6b43375c81007
Alan Gutierrez <alan@prettyrobots.com>
2017-04-28 09:26:13 -0500

    Remove unnecessary indent.

--- run.js
+++ run.js
@@ -36,63 +36,61 @@ var run = cadence(function (async, program) {
 
     // Happens often enough that we shouldn't freak out.
     var operation = cadence(function (async, envelope) {
-        async(function () {
-            async.forEach(function (program) {
+        async.forEach(function (program) {
+            async(function () {
+                shebang(process.platform, program, [], async())
+            }, function (_program, parameters) {
+                var executable = spawn(_program, parameters, {})
+
+                var timer = null
+
+                emit('run')
+
                 async(function () {
-                    shebang(process.platform, program, [], async())
-                }, function (_program, parameters) {
-                    var executable = spawn(_program, parameters, {})
+                    var delta = new Delta(async()), bailed = false, planned = false, plan
 
-                    var timer = null
-
-                    emit('run')
-
-                    async(function () {
-                        var delta = new Delta(async()), bailed = false, planned = false, plan
-
-                        byline.createStream(executable.stderr).on('data', function (line) {
-                            emit('err', line)
-                        })
-
-                        byline.createStream(executable.stdout).on('data', function (line) {
-                            if (bailed) {
-                                emit('out', line)
-                            } else if (tap.assertion(line)) {
-                                emit('test', line)
-                            } else if (!planned && (plan = tap.plan(line))) {
-                                planned = true
-                                emit('plan', plan.expected)
-                            } else if (tap.bailout(line)) {
-                                bailed = true
-                                emit('bail', line)
-                            } else {
-                                emit('out', line)
-                            }
-                        })
-
-                        delta.ee(executable.stdout)
-                             .ee(executable.stderr)
-                             .ee(executable).on('close')
-                    }, function (code, signal) {
-                        emit('exit', (code == null ? 'null' : code) + ' ' + (signal == null ? 'null' : signal))
-                        clearTimeout(timer)
+                    byline.createStream(executable.stderr).on('data', function (line) {
+                        emit('err', line)
                     })
 
-                    function emit (type, message) {
-                        if (timer) {
-                            clearTimeout(timer)
+                    byline.createStream(executable.stdout).on('data', function (line) {
+                        if (bailed) {
+                            emit('out', line)
+                        } else if (tap.assertion(line)) {
+                            emit('test', line)
+                        } else if (!planned && (plan = tap.plan(line))) {
+                            planned = true
+                            emit('plan', plan.expected)
+                        } else if (tap.bailout(line)) {
+                            bailed = true
+                            emit('bail', line)
+                        } else {
+                            emit('out', line)
                         }
-                        timer = setTimeout(kill, params.timeout ? params.timeout * 1000 : 30000)
-                        stamp(program, type, message)
-                    }
+                    })
 
-                    function kill () {
-                        timer = null
-                        executable.kill()
-                    }
+                    delta.ee(executable.stdout)
+                         .ee(executable.stderr)
+                         .ee(executable).on('close')
+                }, function (code, signal) {
+                    emit('exit', (code == null ? 'null' : code) + ' ' + (signal == null ? 'null' : signal))
+                    clearTimeout(timer)
                 })
-            })(envelope.body)
-        })
+
+                function emit (type, message) {
+                    if (timer) {
+                        clearTimeout(timer)
+                    }
+                    timer = setTimeout(kill, params.timeout ? params.timeout * 1000 : 30000)
+                    stamp(program, type, message)
+                }
+
+                function kill () {
+                    timer = null
+                    executable.kill()
+                }
+            })
+        })(envelope.body)
     })
 
     var turnstile = new Turnstile
